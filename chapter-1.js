39cb54d5b319069b93502c1211b0fd95cee9f34b
vipin.lahoti <vipi.nsl2787@gmail.com>
2022-08-05 16:58:04 +0530

    initial commit

---
+++ chapter-1.js
@@ -0,0 +1,61 @@
+const todoForm = document.querySelector('[data-form="todo-form"]');
+const todoList = document.querySelector('[data-form="todo-list"]');
+
+/**
+ *
+ * @param {object} formData
+ */
+const buildTasks = (formData) => {
+  // List key/value pairs
+  for (let [name, value] of formData) {
+    const setTask = {
+      id: Math.floor(Math.random() * Date.now()),
+      status: false,
+      [name]: value,
+    };
+    setTasks(setTask);
+  }
+};
+
+/**
+ *
+ * @param {tasks} lists of task added
+ */
+const setTasks = (tasks) => {
+  // create list
+  const createList = document.createElement("li");
+
+  const createItem = document.createElement("span");
+  createItem.textContent = tasks["task"];
+
+  // delete button
+  const deleteItem = document.createElement("button");
+  deleteItem.textContent = "delete";
+  deleteItem.setAttribute("id", tasks["id"]);
+
+  createList.append(createItem);
+  createList.append(deleteItem);
+  todoList.append(createList);
+
+  // delete item
+  deleteItem.addEventListener("click", (event) => {
+    const itemToBeDelete = event.target.id;
+    console.log(
+      "🚀 ~ file: chapter-1.js ~ line 43 ~ deleteItem.addEventListener ~ itemToBeDelete",
+      itemToBeDelete
+    );
+  });
+};
+
+/**
+ *
+ * @param {object} event
+ */
+todoForm.onsubmit = (event) => {
+  // prevent page to reload
+  event.preventDefault();
+  // initialize FormData
+  const formData = new FormData(todoForm);
+  buildTasks(formData);
+  todoForm.reset();
+};
